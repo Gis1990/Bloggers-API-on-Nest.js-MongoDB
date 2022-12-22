a1c2a22449b51549d20efa35498f8c01488b8130
Gis1990 <101275158+Gis1990@users.noreply.github.com>
2022-12-22 18:35:40 +0100

    v 0.12 added devices endpoints

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from "./app.module";
 import mongoose from "mongoose";
 import { BadRequestException, ValidationPipe } from "@nestjs/common";
 import { HttpExceptionFilter } from "./exception.filter";
-import * as cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 export async function runDb(mongoUri: string) {
     try {

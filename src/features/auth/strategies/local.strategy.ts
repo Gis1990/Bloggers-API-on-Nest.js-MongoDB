10f69c754bac8564c5722040ebae4e801f15efbb
Gis1990 <101275158+Gis1990@users.noreply.github.com>
2022-12-25 10:20:40 +0100

    v 0.14.5 fixed  erorrs in security endpoint

--- src/features/auth/strategies/local.strategy.ts
+++ src/features/auth/strategies/local.strategy.ts
@@ -20,6 +20,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
             request.ip,
             request.headers["user-agent"],
         );
+        console.log("user", user);
         if (user) {
             return user;
         } else {
